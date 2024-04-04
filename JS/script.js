eae74b6e966a0a6739a7fae9da54dc2ec70c8b62
kaziarman23 <kaziarman@proton.me>
2024-04-04 17:36:06 +0600

    project accomplished

--- JS/script.js
+++ JS/script.js
@@ -3,7 +3,7 @@ const form = document.querySelector("form");
 const input = document.querySelector(".input");
 const searchBtn = document.querySelector(".searchBtn");
 const wrapper = document.querySelector(".wrapper");
-const img = document.querySelector(".img");
+const moreBtnEl = document.querySelector(".moreBtn");
 
 let page = 1;
 let inputData = "";
@@ -13,18 +13,40 @@ async function fetchingData() {
     const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${API}`;
     const rawDeta = await fetch(url);
     const deta = await rawDeta.json();
-    console.log(deta)
+    if (page === 1) {
+        wrapper.textContent = "";
+    }
 
-    const results = deta.results
-    console.log(results)
+    const results = deta.results;
+    results.map(function (result) {
+        const imageWrapper = document.createElement("div");
+        const image = document.createElement("img");
+        image.src = result.urls.small_s3;
+        image.alt = result.alt_description;
+        const imgLink = document.createElement("a");
+        imgLink.href = result.links.html;
+        imgLink.target = "_blank";
+        imgLink.textContent = result.alt_description;
 
+        imageWrapper.setAttribute("class", "imgs");
+        imageWrapper.appendChild(image);
+        imageWrapper.appendChild(imgLink);
+        wrapper.appendChild(imageWrapper);
+    });
 
-
-    //clearing input deta
-    input.value = ""
+    page++;
+    console.log(page);
+    if (page > 1) {
+        moreBtnEl.style.display = "block";
+    }
 }
 
 form.addEventListener("submit", function (event) {
     event.preventDefault();
+    page = 1;
+    fetchingData();
+});
+
+moreBtnEl.addEventListener("click", function () {
     fetchingData();
 });
